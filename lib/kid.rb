4f0eb679cb7d628ea633de7733dae0991e5343a8
Adella <adellakenn@gmail.com>
2019-06-13 22:30:56 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,12 +2,5 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
- include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
-  
+  extend MetaDancing
 end
