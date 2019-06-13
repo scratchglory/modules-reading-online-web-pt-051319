fa051cd9e2eed5cc27d32900fc8ccaa9de34b737
Adella <adellakenn@gmail.com>
2019-06-13 22:28:20 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,13 +2,20 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
-class Dancer
-    extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-  
-  attr_accessor :name 
-  
-  def initialize(naem)
-    @name = name
+module Dance
+  def twirl
+    "I'm twirling!"
+  end
+ 
+  def jump
+    "Look how high I'm jumping!"
+  end
+ 
+  def pirouette
+    "I'm doing a pirouette"
+  end
+ 
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all."
   end
 end
